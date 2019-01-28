ab5bc4ccf32b16964ffd4267bfa20e89c5705091
Taylor Clark <taylor.clark@varidesk.com>
2019-01-28 16:17:58 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    //SMOOTH SCROLL
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+
+        var target = $(this.hash);
+
+        if (target.length) {
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 150
+            }, 100);
+        }
+    });
+
+    //PARALLAX
+    $('.parallax').parallax();
+});
