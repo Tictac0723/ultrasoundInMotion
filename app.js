777ccee7369391fe2375d53cb3b6fadeb3b031f8
Tictac0723 <taylorclark0723@gmail.com>
2019-01-30 20:52:34 -0600

    add map, slider, faqs

--- app.js
+++ app.js
@@ -17,4 +17,31 @@ $(document).ready(function () {
 
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems);
+
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: true,
+        centerMode: true,
+        slidesToShow: 3,
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+
+        responsive: [{
+                breakpoint: 990,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
 });
